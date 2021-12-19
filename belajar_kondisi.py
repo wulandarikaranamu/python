46b871d12fb0fa9cf5e4816db57e7b4e1329ce7f
wirrnto_aw <antowiranto47@gmail.com>
2021-12-19 18:40:33 +0800

    python

---
+++ belajar_kondisi.py
@@ -0,0 +1,43 @@
+#belajar kondisi
+
+nilai = 18
+
+if nilai > 15:
+    print("cetak kondisi ini jika benar")
+
+print(nilai)
+
+nilai = 8
+
+if nilai < 9:
+    print("cetak kondisi ini jika benar")
+
+print(nilai)
+
+
+if nilai >= 10:
+    print("cetak kondisi ini jika benar")
+if nilai <= 10:
+    print("cetak kondisi ini jika benar")
+
+print(nilai)
+
+
+nilai = 12
+
+if nilai > 14:
+    print("cetak kondisi ini jika bear")
+
+else:
+    print("cetak kondisi ini jika salah")
+
+print(nilai)
+
+if nilai > 5 :
+    print("cetak kondisi ini jika if benar")
+else nilai < 5 :
+    print("cetak kondisi ini jika else benar")
+    print(nilai)
+ 
+
+
